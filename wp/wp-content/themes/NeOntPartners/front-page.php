8db4949125bfdcfc9c06db44e8d1f9e1ccd2f9c7
Joshua Vandermost <jvandermost658@gmail.com>
2020-05-25 15:53:00 -0400

    temp

--- wp/wp-content/themes/NeOntPartners/front-page.php
+++ wp/wp-content/themes/NeOntPartners/front-page.php
@@ -9,15 +9,7 @@
     get_header();
 ?>
 <main>
-    <section>
-    <p>
 <?php
-    if (have_posts()){ 
-        the_post();
-        echo get_the_content();
-    }
-    ?> </p> <?php
-
     if(is_user_logged_in()){
         ?>
             <h2>Welcome back <?php echo esc_html(wp_get_current_user()->user_login);?> </h2>
@@ -33,6 +25,7 @@
             <button id='login-btn'>Log In</button>
             <p>not a partner? <a>sign in</a></p>
         </form>
+<<<<<<< HEAD
     </section>
         <div class="card-row">
         <?php 
@@ -79,12 +72,44 @@
             ?>
         </div>
     </section>
+=======
+        <?php
+    }
+    ?>
+    <div class="card-row">
+    <?php 
+        $products = wc_get_products(array(
+            'category' => array('partner'),
+            'status' => 'publish'
+        ));
+        $yearRate;
+        $monthRate;
 
-    <section class="additional-partnerships">
-            <!-- this will pulled from a advanced custom field -->
-            <?php echo get_field("additional_partnerships", get_the_ID()) ?>
-    </section>
+        foreach($products as $product){
+                ?>
+            <div
+                data-subID="<?php //echo get_field('price', $product-get_id())?>"
+                class="card-subscription
+                        <?php echo (get_field('order', $product->get_id()));?>"
+                        style="order: <?php echo get_field('order', $product->get_id());?>;">
+                <h2><?php echo $product->get_title();?></h2>
+                <p><?php echo $product->get_description();?></p>
+                <h5><?php
+                if ($product->is_type('variable')){
+                    foreach($product->get_available_variations() as $variation){
+                        //print_r($variation['attributes']['attribute_pay-period']);
+                        if($variation['attributes']['attribute_pay-period'] == 'month'){
+                            $monthRate = $variation["display_price"];
+                        } else {
+                            $yearRate = $variation['display_price'];
+                        }
+                    }
+>>>>>>> parent of 97487a0c... setting up initial page
 
+                    echo '<div>$' . esc_html($yearRate) . '<span>/year</span></div>';
+                    echo '<div>$' . esc_html($monthRate) . '/month</div>';
+
+<<<<<<< HEAD
     <section class='card-row'>
             <?php
             $products = wc_get_products(array(
@@ -152,6 +177,57 @@
             }
             ?>
     </section>
+=======
+                } else {
+                    echo "free";
+                }
+                                
+                ?> </h5>
+                <button class="card-sub-select">Select</button>
+            </div>
+                <?php
+            
+        }
+    ?>
+    </div>
+    <div>
+        <?php
+        $products = wc_get_products(array(
+            'category' => 'municipal',
+            'status' => 'public'
+        ));
+        foreach($products as $product){
+        ?>
+            <div
+                data-subID="<?php //echo get_field('price', $product-get_id())?>"
+                class="card-subscription
+                        <?php echo (get_field('order', $product->get_id()));?>"
+                        style="order: <?php echo get_field('order', $product->get_id());?>;">
+                <h2><?php echo esc_html($product->get_title());?></h2>
+                <p><?php echo esc_html($product->get_description());?></p>
+            </div>
+    </div>
+    <div>
+    <?php
+        }
+        
+        print_r(get_categories());
+        $products = wc_get_products(array(
+            'category' => array('plan_event-sport'),
+            'status' => 'public'
+        ));
+        foreach($products as $product){
+        ?>
+            <div
+                data-subID="<?php //echo get_field('price', $product-get_id())?>"
+                class="card-subscription
+                        <?php echo (get_field('order', $product->get_id()));?>"
+                        style="order: <?php echo get_field('order', $product->get_id());?>;">
+                <h2><?php echo esc_html($product->get_title());?></h2>
+                <p><?php echo esc_html($product->get_description());?></p>
+            </div>
+    </div>
+>>>>>>> parent of 97487a0c... setting up initial page
 
     
 <?php   
